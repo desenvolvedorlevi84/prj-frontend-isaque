41398c4a027e67693dcbb866eae5d3127dd30aa1
desenvolvedorlevi84 <isaquelevi@gmail.com>
2023-02-22 18:44:43 -0300

    prj frontend-isaque atualizado

---
+++ fake-api/produto.js
@@ -0,0 +1,8 @@
+[
+    {"nome":"Bateria Tama","img":"https://desenvolvedorlevi84.github.io/prj-frontend/fake-api/img/Bateria_Tama.png","valor":15.300},
+    {"nome":"Baixo Fodera","img":"https://desenvolvedorlevi84.github.io/prj-frontend/fake-api/img/contra_baixo_fodera.png","valor":8.200},
+    {"nome":"Guitarra Fender","img":"https://desenvolvedorlevi84.github.io/prj-frontend/fake-api/img/Guitarra_Fender_Telecaster_Affinity.png","valor":6.800},
+    {"nome":"Mesa Digital Midas","img":"https://desenvolvedorlevi84.github.io/prj-frontend/fake-api/img/mesa-de-som-midas32.png","valor":65.000},
+    {"nome":"Teclado Yamaha XF6","img":"https://desenvolvedorlevi84.github.io/prj-frontend/fake-api/img/teclado_sintetizador_yamaha_motif_xf6.png","valor":9.200},
+    {"nome":"Violao Taylor","img":"https://desenvolvedorlevi84.github.io/prj-frontend/fake-api/img/violao_taylor.png","valor":11.200}
+]
